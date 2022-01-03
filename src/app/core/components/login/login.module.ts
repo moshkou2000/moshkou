94bf8809ff77bf17ee438f00d8e5ae3639500734
Hassan Nasouri <hassan.nasouri@carsome.com>
2022-01-03 22:53:46 +0800

    added login

--- src/app/core/components/login/login.module.ts
+++ src/app/core/components/login/login.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, LoginRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    LoginRoutingModule,
+    ReactiveFormsModule,
+  ],
   declarations: [LoginComponent],
 })
 export class LoginModule {}
